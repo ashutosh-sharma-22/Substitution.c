c044d0b33fa203aa4585b023e19996fb15a8a124
Ariel Leon Socio Bonfim <104951987+arielbonfim@users.noreply.github.com>
2022-06-26 01:36:00 -0300

    Create substitution.c

---
+++ substitution.c
@@ -0,0 +1,87 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int validation(string key);
+int substitution(string text, string key);
+
+int main(int argc, string argv[])
+{
+
+    if (argc != 2)//aceita apenas um argumento
+    {
+        printf("one arg\n");
+        return 1;
+    }
+    if (validation(argv[1]))//invoca validação
+    {
+        string text = get_string("Text: ");
+        string chave = argv[1];
+        substitution(text, chave);
+        return 0;
+    }
+    return 1;
+}
+int validation(string key)//função validação
+{
+    int len = strlen(key);
+
+    if (len != 26)
+    {
+        printf("Key must contain 26 characters.\n");
+        return false;
+    }
+
+    for (int i = 0; i < len; i++)
+    {
+
+        if ((!isalpha(key[i])))
+        {
+            printf("Key must only contain alphabetic characters.\n");
+            return false;
+        }
+
+        for (int j = 0, m = len; j < m; j++)
+        {
+            for (int k = j + 1, o = len; k < o; k++)
+            {
+                if (key[j] == key[k] || key[j] + 32 == key[k] + 32 || key[j] - 32 == key[k] - 32)
+                {
+                    printf("Key must not contain repeated characters.\n");
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+int substitution(string text, string key)
+{
+    printf("ciphertext:");
+    for (int i = 0, n = strlen(text); i < n; i++)//percorre o texto
+    {
+        char letter = text[i]; //letter = letra atual
+        int nl = letter;//nl = número correspondente a letra ascii
+        if (nl >= 65 && nl <= 90)//Confere maiuscula
+        {
+            /*codifica texto */
+            char converte = toupper(key [nl - 65]);
+            printf("%c", converte);
+        }
+        else if (letter >= 'a' && letter <= 'z')//Confere minuscula
+        {
+             /*codifica texto */
+             char converte = tolower(key [nl - 97]);
+             printf("%c", converte);
+
+        }
+        else
+        {
+            printf("%c", letter);
+
+        }
+    }
+    printf("\n");
+    return 1;
+}
